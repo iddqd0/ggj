e9036963e78250d5f3536b92955f5b9a81b8a3a5
Fogsight <fogsight@gmsil.com>
2017-01-20 00:16:42 +0200

    Jumbled Mess Hoorah

---
+++ Assets/Scripts/Data/PrefabManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabManager : MonoBehaviour
+{
+
+    public GameObject Player;
+    public GameObject Wave;
+
+    enum PrefabTags
+    {
+        Player,
+        Wave,
+        Count
+    }
+}
--- Assets/Scripts/GameMain.cs
+++ Assets/Scripts/GameMain.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Collections.Generic;
 
-public class GameMain : MonoBehaviour {
+public class GameMain : MonoBehaviour
+{
+    public static GameMain Script;
+    public Transform LevelMap;
+    public static Dictionary<string, Wave> Waves = new Dictionary<string, Wave>();
+    Coroutine mainLoop;
+    Coroutine animateWaves;
+    int numberOfWaves = 3;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Awake()
+    {
+        Script = this;
+    }
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    public void StartGame()
+    {
+        mainLoop = StartCoroutine(MainLoop());
+    }
+
+    private IEnumerator MainLoop()
+    {
+        while (true)
+        {
+            //Animate waves
+            animateWaves = StartCoroutine(AnimateWaves());
+            yield return new WaitForSeconds(.01f);
+        }
+    }
+
+    private IEnumerator AnimateWaves()
+    {
+        //Create waves
+
+
+        while (true)
+        {
+
+            yield return new WaitForSeconds(.01f);
+        }
+    }
 }
